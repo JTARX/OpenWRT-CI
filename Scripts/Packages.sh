7c686e1d06a6abca4b1715839128bb5a7a7f661c
17kccc <357365498@qq.com>
2024-09-27 21:46:31 +0800

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+echo >> feeds.conf.default
+echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
+./scripts/feeds update istore
+./scripts/feeds install -d y -p istore luci-app-store
+
+
 #安装和更新软件包
 UPDATE_PACKAGE() {
 	local PKG_NAME=$1
